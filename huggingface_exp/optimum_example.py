1a083c6eabada665576aadafbcdcf3e3fe4806f3
sarareg0412 <sara98.regali@gmail.com>
2023-10-23 17:15:13 +0200

    started optimum study

--- huggingface_exp/optimum_example.py
+++ huggingface_exp/optimum_example.py
@@ -2,13 +2,16 @@ from datasets import load_dataset
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 from neural_compressor.config import AccuracyCriterion, PostTrainingQuantConfig, TuningCriterion
 import evaluate
-
+from optimum.intel.neural_compressor import INCQuantizer
+# Example taken from https://huggingface.co/blog/intel
+# Load model and dataset
 model_name = "juliensimon/distilbert-amazon-shoe-reviews"
 model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=5)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 eval_dataset = load_dataset("prashantgrao/amazon-shoe-reviews", split="test").select(range(300))
 
 
+# Create evaluation function to compute model metrics on the evaluation dataset.
 def eval_func(model):
     task_evaluator = evaluate.evaluator("text-classification")
     results = task_evaluator.compute(
@@ -22,6 +25,7 @@ def eval_func(model):
     return results["accuracy"]
 
 
+# Set up quantization configuration
 tuning_criterion = TuningCriterion(max_trials=10)
 accuracy_criterion = AccuracyCriterion(tolerable_loss=0.05)
 # Load the quantization configuration detailing the quantization we wish to apply
@@ -30,3 +34,9 @@ quantization_config = PostTrainingQuantConfig(
     accuracy_criterion=accuracy_criterion,
     tuning_criterion=tuning_criterion,
 )
+
+# The directory where the quantized model will be saved
+save_dir = "./model_inc"
+quantizer = INCQuantizer.from_pretrained(model=model, eval_fn=eval_func)
+# Quantize and save the model
+quantizer.quantize(quantization_config=quantization_config, save_directory=save_dir)
