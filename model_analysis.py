78b8b1ba6254c92df0722aec5acc8585759c5ccd
sarareg0412 <sara98.regali@gmail.com>
2023-11-28 12:00:53 +0100

    Added check for models with dataset info in metadata

--- model_analysis.py
+++ model_analysis.py
@@ -5,7 +5,8 @@ from huggingface_hub import HfApi, ModelFilter  # api to interact with the hub
 
 N_MODELS = 5
 hf_api = HfApi()
-
+categories = ["multi-modal","computer-vision","natural-language-processing","audio","tabular","reinforcement-learning"]
+category = categories[3]
 # The dictionary has categories as keys and the list of tasks associated with them as values.
 category_dict = {"multi-modal": ["feature-extraction", "text-to-image", "image-to-text", "text-to-video",
                                  "visual-question-answering",
@@ -49,24 +50,25 @@ def get_models_of_task(task):
 
 
 def get_models_of_category(list_tasks):
-    # We reduce the list of models of different categories in a single 1D list
+    # We reduce the list of models of different tasks per category, in a single 1D list
     models = reduce(lambda x, y: x + y, [get_models_of_task(task) for task in list_tasks], [])
-    #Select only models that have datasets in their metadata
-    models = list(filter(lambda x: (getattr(x, "cardData", None) is not None), models))
-    #TODO
-    models = list(filter(lambda x: (getattr(x.cardData, "dataset", None) is not None), models))
-    # Return all unique models since the same models could belong to different tasks
+    # Take out duplicate models since the same model could belong to different tasks of the same category
     seen = set()
     models = [mod for mod in models if mod.id not in seen and not seen.add(mod.id)]
+    # Select only models that have datasets info in their cardData metadata
+    models = list(filter(lambda x: ("datasets" in x.cardData), filter(lambda x: (getattr(x, "cardData", None) is not None), models)))
+    #TODO add double check of existing datasets
+    return models
+
 
 # We get the list of top models for a list of tasks of the same category.
-models = get_models_of_category(list_tasks=category_dict["computer-vision"])
+models = get_models_of_category(list_tasks=category_dict[category])
 # We filter out models with 0 likes or downloads and turn everything into a list. We add the
 # "ratio" parameter to the model, representing the ratio between #likes/#downloads
 # Sorting phase based on likes,downloads or ratio. TBD with our own metric
 models.sort(key=lambda x: x.likes, reverse=True)
-print("Top {} models. Total number of unique models: {}".format(N_MODELS, len(models)))
-print("Model name\t\t\tlikes\tdownloads\tlibrary\ttask\tdatasets")
+print("Top {} models for category {}.".format(N_MODELS, category))
+print("Model name\t\t\t\t\tlikes\tdownloads\tlibrary\ttask\tdatasets")
 for model in models[:N_MODELS]:
-    print("{}\t{}\t{}\t{}\t{}\t{}".format(model.modelId, model.likes, model.downloads, model.task, getattr(model,"library_name","None"),
-                                          model.tags, model.card_data.datasets))
+    print("{}\t{}\t{}\t{}\t{}\t{}".format(model.modelId, model.likes, model.downloads, getattr(model,"library_name","None")
+                                          ,model.task, model.cardData["datasets"], ))
