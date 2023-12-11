dc75c2da683d83dd09981be26d9226718725149c
sarareg0412 <sara98.regali@gmail.com>
2023-12-11 15:13:01 +0100

    Loop for energy measurements for quantization

--- dynamic_quant.py
+++ dynamic_quant.py
@@ -6,7 +6,8 @@ import subprocess
 
 models = []
 # Load model and dataset from csv file
-with open("../computer-vision_{}".format(csv_name)) as file:
+
+with open("../models_csv/computer-vision_{}".format(csv_name)) as file:
     csvReader = csv.reader(file)
     # ['model_name', 'likes', 'downloads', 'category', 'task', 'library', 'dataset', 'dataset_config_name']
     header = csvReader.__next__()
@@ -21,16 +22,15 @@ for i in range(0, len(models), 50):
 
 
 # Quantize only the N_MODELS models of each category:
-#for model_data in top_N_models:
-model_data = top_N_models[0]
-n_experiment = 1
-model_name_formatted = model_data["model_name"].replace("/", "-")
+for model_data in top_N_models:
+    n_experiment = 1
+    model_name_formatted = model_data["model_name"].replace("/", "-")
 
-# The saving directory will be of the type models/computer-vision/model_name_formatted
-save_dir = "models/{}/{}".format(model_data["category"], model_name_formatted)
-energy_output_file = "quantization_energy_output_{}_exp{}".format(model_name_formatted, n_experiment)
-print("Start Quantization for model {}".format(model_data["model_name"]))
-subprocess.run(["energibridge", "-o", "{}".format(energy_output_file),
-                "python", "run_energy.py", "{}".format(save_dir), "{}".format(model_data["library"]), "{}".format(model_data["model_name"])])
-print("End Quantization for model {}, saved to {}".format(model_data["model_name"], save_dir))
-n_experiment += 1
+    # The saving directory will be of the type models/computer-vision/model_name_formatted
+    save_dir = "models/{}/{}".format(model_data["category"], model_name_formatted)
+    energy_output_file = "{}/energy_output_exp{}.csv".format(save_dir, n_experiment)
+    print("Start Quantization for model {}".format(model_data["model_name"]))
+    subprocess.run(["../energibridge", "-o", "{}".format(energy_output_file),
+                    "python", "run_energy.py", "{}".format(save_dir), "{}".format(model_data["library"]), "{}".format(model_data["model_name"])])
+    print("End Quantization for model {}, saved to {}".format(model_data["model_name"], save_dir))
+    n_experiment += 1
