1989696c36db34c5a444924940db052b18289c89
sarareg0412 <sara98.regali@gmail.com>
2023-12-15 15:58:38 +0100

    Changing inference logic

--- analysis/run_inference.py
+++ analysis/run_inference.py
@@ -3,8 +3,9 @@ import sys
 
 from utils import *
 from transformers import pipeline
-from datasets import load_dataset, load_metric
+from datasets import load_dataset
 import evaluate
+from evaluate import evaluator
 from transformers import AutoImageProcessor
 from sentence_transformers import SentenceTransformer
 from huggingface_hub import from_pretrained_keras
@@ -44,34 +45,49 @@ def run_evaluation_from_line(quantized, line):
         case _:
             processor = None
 
-    pipe = pipeline(model_data["task"], model=model, image_processor=processor)
-    acc = load_metric("accuracy")
-    # Initialize lists to store references and predictions for accuracy evaluation
-    references = []
-    predictions = []
+    use_evaluator = False
+    if use_evaluator:
+        task_evaluator = evaluator(model_data["task"])
+        # Evaluate the model (performs inference too)
+        eval_results = task_evaluator.compute(
+            model_or_pipeline=model,
+            data=data,
+            label_column=data.column_names[-1],  # We assume the last column is the labels one
+            label_mapping=model.config.label2id,    # Extremely important
+            feature_extractor=processor,
+            tokenizer=processor
+        )
+        print(eval_results)
+    else:
+        pipe = pipeline(model_data["task"], model=model, image_processor=processor)
+        acc = evaluate.load("accuracy")
+        # Initialize lists to store references and predictions for accuracy evaluation
+        references = []
+        predictions = []
 
-    # Iterate through the validation set or any other split
-    for example in data:
-        # Load object and label truth label from the dataset
-        object = example[data.column_names[0]]  # Assume the object column name is the first one
-        label = example[data.column_names[-1]]  # Assume the label column name is the last one
+        # Iterate through the validation set or any other split
+        for example in data:
+            # Load object and label truth label from the dataset
+            object = example[data.column_names[0]]  # Assume the object column name is the first one
+            label = example[data.column_names[-1]]  # Assume the label column name is the last one
 
-        # Infer the object label using the model
-        prediction = pipe(object)
+            # Infer the object label using the model
+            prediction = pipe(object)
 
-        # Assuming 'prediction' contains class probabilities or labels
-        predicted_label = prediction[0][data.column_names[-1]] if isinstance(prediction, list) \
-                                                               else prediction[data.column_names[-1]]
+            # Since there might be multiple labels with multiple scores associated, we get the first one.
+            predicted_label = prediction[0]['label'] if isinstance(prediction, list) \
+                                                                   else prediction['label']
 
-        # Append ground truth label and predicted label for accuracy evaluation
-        references.append(label)
-        predictions.append(predicted_label)
+            # Append ground truth label and predicted label for accuracy evaluation
+            references.append(label)
+            predictions.append(model.config.label2id[predicted_label])  # Map the predicted label using the model's label2id attribute
 
-    # Calculate accuracy using the loaded accuracy metric
-    accuracy_score = acc.compute(predictions=predictions, references=references)
+        # Calculate accuracy using the loaded accuracy metric
+        accuracy_score = acc.compute(predictions=predictions, references=references)
 
-    print(f"Inference accuracy is : {accuracy_score}")
+        print(f"Inference accuracy is : {accuracy_score}")
+
 
 if __name__ == "__main__":
     run_evaluation_from_line(sys.argv[1], sys.argv[2])
-
+    #run_evaluation_from_line("True","nateraw/vit-base-beans,10,2023,computer-vision,image-classification,transformers,beans,")
